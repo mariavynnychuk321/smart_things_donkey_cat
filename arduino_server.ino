1ce0a21fb2ce3781d89fff119e272550d4116532
IhorHrynyk <grynyk_igor@ukr.net>
2018-04-29 22:00:27 +0300

    test connection (arduino side)

---
+++ arduino_server.ino
@@ -0,0 +1,16 @@
+String data;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+
+  while (Serial.available()) {
+    data = Serial.readString();
+  }
+  
+  if (data == "9:40 10:40 11:40 9:40 10:40 11:40") digitalWrite(13, HIGH);
+}
+
