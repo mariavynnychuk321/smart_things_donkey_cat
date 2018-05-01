d528a9a3d802ef95f6b97c71c67da28831dc967a
IhorHrynyk <grynyk_igor@ukr.net>
2018-05-02 00:54:59 +0300

    fixed problems with state ON\OFF

--- cat_feeder.ino
+++ cat_feeder.ino
@@ -6,18 +6,14 @@
 volatile short button = 2;
 short servo = 11;
 // state
-volatile bool isEmpty = false;
+volatile bool isEmpty = true;
 
-int magicNum = 3;     				// [how much times a day cat eats] - 1
+int magicNum = 0;     				// [how much times a day cat eats]
 short i = 0;        				// how much times worked
-int feedTime[10][2];  				// array with feed time
-  
-void getTiming() {
-	
-}
+int feedTime[10];	  				// array with feed time  
 
 // react on button presure changes
-void isr() {
+void isr1() {
   	
   	if (!isEmpty) { 				// when button is PRESSED (food is there)
   		unsigned short zeros = 1; 	// need to react on LOW
@@ -36,8 +32,12 @@ void isr() {
 			isEmpty = true; 		// means button UNPRESSED -> food is over  
 		}
   	}
+  
+}
 
-  	else if (isEmpty) {				// when button UNPRESSED (there's no food)
+void isr2() {
+
+	if (isEmpty) {				// when button UNPRESSED (there's no food)
   		unsigned short zeros = 0; 
 		unsigned short ones = 1; 	// need to react on HIGH
 
@@ -54,11 +54,12 @@ void isr() {
 			isEmpty = false; 		// means button PRESSED again -> food is there  
 		}
   	}
-  
+
 }
 
 // controling servo
 int prewAngle = 0;
+
 void moveServo(int degree){
   
 	int start = millis();
@@ -76,20 +77,20 @@ void moveServo(int degree){
 }
 
 void giveFood() {
-  Serial.println("inside giveFood()");
+
+  	Serial.println("inside giveFood()");
 	
 	if (isEmpty) {
 
 		// need delay on some time to not throw food straight on cat 
-		// + 10 mins i think
-
-		// open container
-		moveServo(120);
-		
-		// when enough food close
-		if (!isEmpty) {
-			moveServo(0);
+		delay(1000);
+    
+		while (isEmpty) {
+		  moveServo(120);
 		}
+
+    delay(1000);
+    moveServo(0);
 
 	}
 
@@ -99,39 +100,37 @@ void setup() {
 
 	pinMode(button, INPUT);
 	pinMode(servo, OUTPUT);
-  	attachInterrupt(0, isr, LOW);
-
+  	
+  	attachInterrupt(0, isr1, LOW);
+	attachInterrupt(0, isr2, HIGH);
+  	
   	Serial.begin(9600);
+
 }
 
+
 void loop () {
-	
-/*
-  	Serial.print(feedTime[0][0]);
-  	Serial.print(":");
-  	Serial.print(feedTime[0][1]);
-  	Serial.println(); 
-*/
 
-//  TEST
-//  feedTime[0][0] = 11;
-//  feedTime[0][1] = 21;
-
+  //delay(5000);
+  
 	tmElements_t currentTime;
+ //  TEST
+//feedTime[0] = 0;
+//feedTime[1] = 52;  
   
 	// read time
 	if (RTC.read(currentTime)) {
 
-		if (currentTime.Hour == feedTime[i][0] && currentTime.Minute == feedTime[i][1]) {
+		if (currentTime.Hour == feedTime[i] && currentTime.Minute == feedTime[i + 1]) {
 			
 			giveFood();
-			i++;
+			i += 2;
       
-      		if (i == magicNum) {
+      		if (i == magicNum - 1) {
         		i = 0;
       		}
 		  	
-		  	Serial.println(i);
+		  	// Serial.println(i);
       
 		}
 
@@ -140,7 +139,7 @@ void loop () {
         	Serial.println(i);
 	    	Serial.print(currentTime.Hour);
 	    	Serial.print(":");
-	    	Serial.print(currentTime.Minute);
+	    	Serial.println(currentTime.Minute);
   		}
 
 	}
@@ -149,16 +148,13 @@ void loop () {
 		
 		if (RTC.chipPresent()) {
 	    	Serial.println("The DS1307 is stopped.  Please run the SetTime");
-	    	Serial.println("example to initialize the time and begin running.");
-	    	Serial.println();
-		} 
+	    } 
 
 		else {
 	      	Serial.println("DS1307 read error!  Please check the circuitry.");
-	      	Serial.println();
 	    }
     	
-    	delay(9000);
+    	delay(5000);
   	}
 
 //   delay(1000);
