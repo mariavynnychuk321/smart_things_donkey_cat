f45f7e1c87440e0e493ceaad8d534829ddf44ac6
IhorHrynyk <grynyk_igor@ukr.net>
2018-05-01 04:20:35 +0300

    UPD: data is parsing to array + count times cat eats

--- arduino_server.ino
+++ arduino_server.ino
@@ -1,4 +1,10 @@
+#include <string.h>
+
 String data;
+bool toReadOrNotToRead = false;
+
+int magicNum = 0;
+int feedTime[10] = {-9, -9, -9, -9, -9, -9, -9, -9, -9, -9};
 
 void setup() {
   Serial.begin(9600);
@@ -7,10 +13,28 @@ void setup() {
 
 void loop() {
 
-  while (Serial.available()) {
-    data = Serial.readString();
-  }
-  
-  if (data == "9:40 10:40 11:40 9:40 10:40 11:40") digitalWrite(13, HIGH);
-}
+    if (toReadOrNotToRead == false) {
 
+        while (Serial.available()) {
+             data = Serial.readString();
+        }
+    
+        //if (data == "9 40 10 40 11 40") digitalWrite(13, HIGH);
+
+        // parse data
+        for(int i = 0; i < 10; i++) {
+            int index = data.indexOf(" ");
+            feedTime[i] = atoi(data.substring(0, index).c_str());
+            data = data.substring(index + 1);
+        }
+
+        // count how mush times cat eats
+        for(int i = 0; i < 9; i += 2) {
+            if (feedTime[i] != 0 && feedTime[i + 1] != 0)
+              magicNum ++;
+        }
+
+        // if(magicNum == 3) digitalWrite(13, HIGH);
+    }
+  
+}
